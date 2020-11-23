c698abb6c25202b0471f246433ac9c7c08e52b28
Alan Duong <alanduong07@gmail.com>
2020-11-23 14:14:40 -0600

    Learned new Javascript terminology and best practices

--- test.js
+++ test.js
@@ -1,1 +1,8 @@
-alert("Welcome to Javascript!");
+"use script";
+//this is a comment test :)
+/* and this is a test of a 
+multiline comment! */
+
+//camel case is commonly used for multi word variables
+let myMessage = "This is Javascript!";
+alert(myMessage);
